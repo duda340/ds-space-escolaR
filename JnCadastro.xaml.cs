bc8846fbc348007f40eb93d063cd45e2cb18fa58
eduarda R <eduardarufino340@gmail.com>
2022-04-12 07:41:35 -0400

    atualização

---
+++ JnCadastro.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CadastroE
+{
+    /// <summary>
+    /// Lógica interna para JnCadastro.xaml
+    /// </summary>
+    public partial class JnCadastro : Window
+    {
+        public JnCadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
